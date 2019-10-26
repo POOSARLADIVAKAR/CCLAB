e6d53e6df7273efcf3ded484035f236a5bf32ccb
nagasaibharath <nagasaibharath912@gmail.com>
2019-10-26 15:46:24 +0530

    navbar tried

--- src/components/Complaints.js
+++ src/components/Complaints.js
@@ -10,10 +10,36 @@ class Complaints extends Component{
             console.log(res);
         })        
     }
+
+    // componentDidUpdate(){
+    //     document.addEventListener('DOMContentLoaded', function() {
+    //         var elems = document.querySelectorAll('.sidenav');
+    //         var instances = navbar-fixed.Sidenav.init(elems, options);
+    //     });
+
+    // }
+    
     render(){
         return (
-            <div>
-                <NavBar/>
+            <div className="navbar-fixed">
+                <nav>
+                    <div className="nav-wrapper">
+                    <a href="#" className="brand-logo center">Logo</a>
+                    <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="sass.html">Sass</a></li>
+                        <li><a className="active" href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                    </div>
+                </nav>
+
+                <ul className="sidenav" id="mobile-demo">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">Javascript</a></li>
+                    <li><a href="mobile.html">Mobile</a></li>
+                </ul> 
             </div>
         )
     }
