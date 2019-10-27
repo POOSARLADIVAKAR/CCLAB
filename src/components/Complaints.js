b45b1d9a0c5bfd7faa7baf8aec073ca37bf27323
Divakar <divakarpoosarla123@gmail.com>
2019-10-27 17:51:09 +0530

    Complaints page with side Nav CSS

--- src/components/Complaints.js
+++ src/components/Complaints.js
@@ -1,37 +1,46 @@
-// import React, {Component} from 'react'
-// import {Row,Col,Container} from 'react-bootstrap'
-// import NavBar from './NavBar'
-// import axios from 'axios'
-// import M from 'materialize-css';
-// import {NavLink} from 'react-router-dom'
-// class Complaints extends Component{
-//     componentDidMount(){
-//         console.log("in complaints")  
-//         axios.get('http://localhost:4000/').then((res)=>{
-//             console.log(res);
-//         })
-//     }
+import React, {Component} from 'react'
+import NavBar from './NavBar'
+import {Nav} from 'react-bootstrap'
+import axios from 'axios'
+import M from 'materialize-css';
+import './../cssfiles/Complaints.css'
+class Complaints extends Component{
+  //add code for token removal
+    componentDidMount(){
+        console.log("in complaints")  
+        // axios.get('http://localhost:4000/').then((res)=>{
+        //     console.log(res);
+        // })
+    }
     
-//   render(){
-//     return (
-//       <div>
-//         <nav>Hello world</nav>
-//         <ul id="slide-out" class="sidenav" >
-//         <li><div class="user-view">
-//             <div class="background">
-//               <img src="images/office.jpg"></img>
-//               </div>
-//         </div></li>
-//         <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-//         <li><a href="#!">Second Link</a></li>
-//         <li><div class="divider"></div></li>
-//         <li><a class="subheader">Subheader</a></li>
-//         <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-//         </ul>
-//         <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>    
-//       </div>
-//     )
-//   }
-// }
+  render(){
+    return (
+      <div>
+        <NavBar/>
+        <div className="parent"> 
+          <div className="tab">
+              <button className="tablinks" >Log</button>
+              <button className="tablinks" >Solved</button>
+          </div>
 
-// export default Complaints;
+          <div id="search_space" className="tabcontent">
+            <div className = "searchBar"> {/*float top and bottom*/}
+              <input type="text" placeholder = "Search ..."/>
+              <button type="button" >
+                Hello
+                <i className="fa fa-search"></i>
+              </button>
+            </div>
+            <div className = "bottom-content">
+            {/*<Box/> Load component*/}
+              <h1>hello world</h1>
+              <h2>hello world</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Complaints;
