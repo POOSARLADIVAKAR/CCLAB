edd6d7b7319116037eb427084c164fa89475bcaa
nagasaibharath <nagasaibharath912@gmail.com>
2019-11-05 08:12:38 +0530

    for pulling

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -47,6 +47,45 @@ class Resources extends Component{
                     <li className="flex-item ">D205</li>
                     <li className="flex-item ">D206</li>
                 </ul>
+
+                <div id="myModal" class="modal fade" role="dialog">
+                    <div class="modal-dialog modal-lg modal-dialog-centered">
+
+                        {/* <!-- Modal content--> */}
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" backdrop="static">&times;</button>
+                                <h4 class="modal-title">Modal Header</h4>
+                            </div>
+                            <div class="modal-body">
+                                <form class="form-horizontal">
+                                    <div class="form-group">
+                                        <label class="control-label col-sm-2" for="email">Email:</label>
+                                        <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="email" placeholder="Enter email"></input>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-sm-2" for="pwd">Password:</label>
+                                        <div class="col-sm-10">
+                                        <input type="password" class="form-control" id="pwd" placeholder="Enter password"></input>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-sm-offset-2 col-sm-10">
+                                        <button type="submit" class="btn btn-default">Submit</button>
+                                        </div>
+                                    </div>
+                                </form> 
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
                 <div className="table">
                     <div className="thead">
                         <div className="tr">
@@ -65,6 +104,9 @@ class Resources extends Component{
                             })
                             }
                     </div>
+                    {/* <!-- Trigger the modal with a button --> */}
+                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
                </div>
         );
     }
